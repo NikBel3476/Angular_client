6a7d001035f253dbe9b14601fa3e32b22d5cb3d6
NikBel3476 <nikita.belkin3476@gmail.com>
2021-03-13 21:45:07 +0400

    delete unused modules

--- src/app/server/Server.ts
+++ src/app/server/Server.ts
@@ -1,9 +1,6 @@
 import { io } from "socket.io-client";
-// import { Socket } from 'ngx-socket-io';
 
 import { SETTINGS } from "./Settings";
-
-
 
 
 export class Server {
@@ -33,42 +30,3 @@ export class Server {
     
 
 }
-
-/*
-//const { HOST, PORT, MESSAGES } = SETTINGS;
-
-    const socket = io(`${HOST}:${PORT}`);
-
-    let login;
-
-    // чат
-    // отправка сообщений
-    document.getElementById('sendText').addEventListener(
-        'click', () => {
-            const message = document.getElementById('textLine').value;
-            socket.emit(MESSAGES.SEND_MESSAGE, { message, login });
-            return true;
-        }
-    );
-
-    document.getElementById('reg_button').addEventListener(
-        'click', () => {
-            const name = document.getElementById('reg_input').value;
-            socket.emit(MESSAGES.SEND_LOGIN, { name });
-            return true;
-        }
-    );
-
-    socket.on(MESSAGES.SEND_MESSAGE, data => {
-        const chat = document.getElementById('chat');
-        chat.value += data.login + ': ' + data.message + '\n';
-    });
-
-    socket.on(MESSAGES.SEND_LOGIN, data => {
-        if (data.result) {
-            login = data.result;
-            ui.hideElem('reg_container');
-            ui.showElem('chat_container');
-        }
-    });
-*/
