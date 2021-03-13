c4260ee5576fadd116df88c89457d502d22276b5
NikBel3476 <nikita.belkin3476@gmail.com>
2021-03-13 15:29:56 +0400

    delete calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
 import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent,
     ChatComponent
   ],
   imports: [
