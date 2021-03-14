acb54c6903cfc77ff5cf6370d05aeda311c457c3
NikBel3476 <nikita.belkin3476@gmail.com>
2021-03-14 22:31:02 +0400

    changes

--- src/app/authorization/authorization.component.ts
+++ src/app/authorization/authorization.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Md5 } from 'ts-md5/dist/md5';
 
 import { User } from '../user';
 import { Server } from '../server/Server';
@@ -10,8 +9,6 @@ import { Server } from '../server/Server';
   styleUrls: ['./authorization.component.css']
 })
 export class AuthorizationComponent implements OnInit {
-
-  md5 = new Md5();
 
   server = new Server;
 
@@ -36,28 +33,11 @@ export class AuthorizationComponent implements OnInit {
   }
 
   authorization = () => {
-    const { login, password } = this.user;
-    const num = Math.round(Math.random() * 1000000);
-    if (login && password) {
-      const passHash = this.md5.appendStr(login + password).end();
-      const token = this.md5.appendStr(passHash + String(num)).end();
-      this.server.login({ passHash, token, num});
-    }
+    this.server.login(this.user);
   }
 
   registration = () => {
-    const { nickname, login, password } = this.user;
-    if (nickname && login && password) {
-      const passHash = this.md5.appendStr(login + password).end();
-      const token = this.md5.appendStr(passHash + String(Math.random() * 100000)).end();
-      this.server.registration({ nickname, passHash, token});
-    }
-  }
-
-  printName = () => {
-    console.log(this.user.nickname);
-    console.log(this.user.login);
-    console.log(this.user.password);
+    this.server.registration(this.user);
   }
 
 }
--- src/app/server/Server.ts
+++ src/app/server/Server.ts
@@ -1,9 +1,12 @@
 import { io } from "socket.io-client";
+import { Md5 } from 'ts-md5/dist/md5';
 
 import { SETTINGS } from "./Settings";
+import { User } from '../user';
 
 
 export class Server {
+
     HOST = SETTINGS.HOST;
     PORT = SETTINGS.PORT;
     MESSAGES = SETTINGS.MESSAGES;
@@ -17,16 +20,23 @@ export class Server {
         }
     }
 
-    login = (data: object) => {
-        console.log(data);
-        this.socket.emit(this.MESSAGES.LOGIN, data);
+    login = (user: User) => {
+        const { login, password } = user;
+        const num = Math.round(Math.random() * 1000000);
+        if (login && password) {
+          const passHash = Md5.hashStr(login + password);
+          const token = Md5.hashStr(passHash + String(num));
+          this.socket.emit(this.MESSAGES.LOGIN, { login, passHash, token, num });
+        }
     }
 
-    registration = (data: object) => {
-        console.log(data);
-        this.socket.emit(this.MESSAGES.REGISTRATION, data);
-    }
-
-    
-
+    registration = (user: User) => {
+        const { login, nickname, password } = user;
+        const num = Math.round(Math.random() * 1000000);
+        if (nickname && login && password) {
+          const passHash = Md5.hashStr(login + password);
+          const token = Md5.hashStr(passHash + String(num));
+          this.socket.emit(this.MESSAGES.REGISTRATION, { login, nickname, passHash, token, num });
+        }
+      }
 }
